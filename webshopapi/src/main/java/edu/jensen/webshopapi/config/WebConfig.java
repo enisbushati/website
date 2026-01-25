468ff67980cc9fcc384f3e584d249f330fc7d2d5
nadeemjeee <nadeemjeee@gmail.com>
2026-01-25 19:55:54 +0100

    Add CORS configuration to WebConfig for improved API access

--- webshopapi/src/main/java/edu/jensen/webshopapi/config/WebConfig.java
+++ webshopapi/src/main/java/edu/jensen/webshopapi/config/WebConfig.java
@@ -18,4 +18,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
     }
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(
+                    "http://localhost:3000",
+                    "http://192.168.0.212:3000"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+
 }
